656bebeafb3f4dfac0595c2ac68a9eb2e8649a84
Victor Ebenezer <svehopkins@gmail.com>
2023-05-20 15:08:39 +0100

    add header

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,4 @@
+import Header from "../components/Header";
 import RootLayout from "../components/layout";
 import styled from "styled-components";
 
@@ -5,20 +6,25 @@ const PageContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background: #39cd86;
-  font-family: "Monomaniac One";
+  font-family: "Roboto mono";
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const Header = styled.div`
-  width: 100%;
-  margin-top: 48px;
+const ContentContainer = styled.div`
+  background: #3d7238;
+  width: 80%;
+  max-width: 1164px;
+  font-family: "Roboto mono";
 `;
 
 export default function Home({}) {
   return (
     <RootLayout>
       <PageContainer>
-        Some stuff
-        <Header></Header>
+        <Header />
+        <ContentContainer> Some text </ContentContainer>
       </PageContainer>
     </RootLayout>
   );

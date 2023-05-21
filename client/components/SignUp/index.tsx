e016538b6178b0ea900012715fcedac759400faa
Victor Ebenezer <svehopkins@gmail.com>
2023-05-21 07:35:22 +0100

    sismo re-route fix

--- client/components/SignUp/index.tsx
+++ client/components/SignUp/index.tsx
@@ -9,7 +9,7 @@ const SignUp = () => {
   const { query } = useRouter();
 
   switch (query.step) {
-    case "sismo":
+    case undefined:
       return <SismoSignUp />;
     case "userType":
       return <UserTypeSelection />;

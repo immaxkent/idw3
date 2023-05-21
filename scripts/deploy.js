bcf55335e5786ab3f2bb799f61eb6b58fadfeb7b
iamsahu <praffulsahu@gmail.com>
2023-05-21 09:32:29 +0200

    feat: deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,16 +6,17 @@ async function main() {
   // Deploy Idw3Factory
   const Idw3Factory = await ethers.getContractFactory("Idw3Factory");
   console.log("Deploying Idw3Factory...");
-  const idw3Factory = await Idw3Factory.deploy();
+  const idw3Factory = await Idw3Factory.deploy("0x72243301d793b93727c983b23ff9209e");
+  console.log('here')
   await idw3Factory.deployed();
   console.log("Idw3Factory deployed to:", idw3Factory.address);
 
   // Deploy Idw3Bridge
-  const Idw3Bridge = await ethers.getContractFactory("Idw3Bridge");
-  console.log("Deploying Idw3Bridge...");
-  const idw3Bridge = await Idw3Bridge.deploy(idw3Factory.address);
-  await idw3Bridge.deployed();
-  console.log("Idw3Bridge deployed to:", idw3Bridge.address);
+  // const Idw3Bridge = await ethers.getContractFactory({contract:"Idw3Bridge",arguments:['0x72243301d793b93727c983b23ff9209e']});
+  // console.log("Deploying Idw3Bridge...");
+  // const idw3Bridge = await Idw3Bridge.deploy("0x72243301d793b93727c983b23ff9209e");
+  // await idw3Bridge.deployed();
+  // console.log("Idw3Bridge deployed to:", idw3Bridge.address);
 
 }
 
